161c40840511a40ad6b499d29c1b49e886266ab0
Jorge Perez <jorgito@mit.edu>
2013-11-12 04:34:17 -0500

    extended blocks world

--- pset5/blocksWorld.py
+++ pset5/blocksWorld.py
@@ -1,5 +1,5 @@
 from planner import *
-class GrabBlockFromTable(Action):
+class GrabFromTable(Action):
     actionName = 'grab block from table'
     def resultStateAndCost(self, state, noDel = False):
         (arm,block) = self.args
@@ -18,7 +18,7 @@ class GrabBlockFromBlock(Action):
                                     # Deletes
                                     [('free', arm), ('clear', block), ('on', block1, block2)],
                                     noDel),1)
-class PutBlockOnTable(Action):
+class PutOnTable(Action):
     actionName = 'put on table'
     def resultStateAndCost(self, state, noDel = False):
         (arm,block) = self.args
@@ -38,20 +38,78 @@ class PutBlockOnBlock(Action):
                                     [('clear', block2), ('on', block1,arm)],
                                     noDel),1)
 
-BLOCKS = ['blockA', 'blockB', 'blockC', 'blockD', 'blockE']
-ARMS = ['arm1', 'arm2', 'arm3']
+class SprayPaint(Action):
+    actionName = 'spray paint'
+    def resultStateAndCost(self, state, noDel = False):
+        (block, sprayer, color, arm) = self.args
+        if state.check([('has-color', color, sprayer),('on', sprayer, arm),
+                                    ('on-table', block), ('clear', block)]):
 
-INITIAL = State([('clear', block) for block in BLOCKS] +\
+            return (state.addDelete([('is-color', color, block)],
+                                    # Deletes
+                                    [],
+                                    noDel),1)
+
+class LoadBrush(Action):
+    actionName = 'load brush'
+    def resultStateAndCost(self, state, noDel = False):
+        (brush, can, color, arm) = self.args
+        if state.check([('clean', brush), ('has-color', color, can),('on', brush, arm), 
+                        ('clear', can)]):
+
+            return (state.addDelete([('has-color', color, brush)],
+                                    # Deletes
+                                    [('clean',brush)],
+                                    noDel),1)
+
+class BrushPaint(Action):
+    actionName = 'brush paint'
+    def resultStateAndCost(self, state, noDel = False):
+        (block, color, brush, arm) = self.args
+        if state.check([('has-color', color, brush),('on', brush, arm), ('on-table', block), ('clear', block)]): 
+            return (state.addDelete([('is-color', color, block)],
+                                    # Deletes
+                                    [],
+                                    noDel),1)
+
+class WashBrush(Action):
+    actionName = 'wash brush'
+    def resultStateAndCost(self, state, noDel = False):
+        (brush, color, bucket, arm) = self.args
+        if state.check([('has-color', color, brush),('on', brush, arm),('clear', bucket)]):
+
+            return (state.addDelete([('clean',brush)],
+                                    # Deletes
+                                    [('has-color', color, brush)],
+                                    noDel),1)
+
+
+
+
+
+
+
+BLOCKS = ['blockA', 'blockB', 'blockC', 'blockD', 'blockE']
+ARMS = ['arm1']
+COLOR = ['red','blue','green', 'yellow']
+CANS = ['can1', 'can2', 'can3']
+BRUSHES = ['brush1', 'brush2']
+SPRAYERS = ['sprayer1', 'sprayer2', 'sprayer3']
+BUCKET = ['bucket1']
+
+INITIAL0 = State([('clear', block) for block in BLOCKS] +\
                 [('on-table', block) for block in BLOCKS] +\
                 [('free', arm) for arm in ARMS])
 
 GOAL0 = [('on-table', 'blockA'),('on', 'blockC', 'blockB'), ('on', 'blockB', 'blockA'), \
          ('on-table', 'blockE'), ('on', 'blockD', 'blockE')]
 
-ACTS0 = [GrabBlockFromTable(args) for args in combinations([ARMS,BLOCKS])] +\
+ACTS0 = [GrabFromTable(args) for args in combinations([ARMS,BLOCKS])] +\
         [GrabBlockFromBlock(args) for args in combinations([ARMS,BLOCKS,BLOCKS])] + \
-        [PutBlockOnTable(args) for args in combinations([ARMS,BLOCKS])]+\
+        [PutOnTable(args) for args in combinations([ARMS,BLOCKS])]+\
         [PutBlockOnBlock(args) for args in combinations([ARMS, BLOCKS,BLOCKS])]
+
+INITIAL1 = State([(
 
 block_world = PlanProblem(INITIAL, GOAL0, ACTS0)
 block_world.findPlan(1000)
